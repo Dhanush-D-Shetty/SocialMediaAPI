776bc7b49b335d499789fc5ff4fb4849ac5d93c0
Dhanush D  Shetty <134682792+Dhanush-D-Shetty@users.noreply.github.com>
2024-04-18 16:49:49 +0530

    Update SocialmediaApplication.java

--- socialmedia/src/main/java/com/shetty/socialmedia/SocialmediaApplication.java
+++ socialmedia/src/main/java/com/shetty/socialmedia/SocialmediaApplication.java
@@ -3,7 +3,13 @@ package com.shetty.socialmedia;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Contact;
+import io.swagger.v3.oas.annotations.info.Info;
+
+
 @SpringBootApplication
+@OpenAPIDefinition(info = @Info(title = "Shetty Media", version = "1.0", description = "RestAPI for social media applications", contact = @Contact(name = "Dhanush Shetty",email = "dhanushdinesh29@gmail.com", url = "https://github.com/Dhanush-D-Shetty/SocialMediaAPI")))
 public class SocialmediaApplication {
 
 	public static void main(String[] args) {
